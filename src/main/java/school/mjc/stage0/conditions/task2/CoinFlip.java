8cf1f25063457fbf4ee1ee7c56f6ffb72e63ee51
Dmitrii Dolbik <dda1508@gmail.com>
2022-12-25 15:44:41 +0300

    add solution

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -1,7 +1,16 @@
 package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
+    public static void main(String[] args) {
+        CoinFlip cf = new CoinFlip();
+        cf.throwCoin(500);
+    }
     public void throwCoin(int from1UpTo1000) {
+        if (from1UpTo1000 < 500){
+            System.out.println("Eagle");
+        } else {
+            System.out.println("Tail");
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
+    public static void main(String[] args) {
+        GreatestNumberPrinter gnp = new GreatestNumberPrinter();
+        gnp.printGreatest(90,8);
+    }
+
     public void printGreatest(int first, int second) {
+        if (first >= second){
+            System.out.println(first);
+        } else {
+            System.out.println(second);
+        }
     }
 }
